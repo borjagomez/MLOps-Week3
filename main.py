f666c24d50aab806414a1ac0d2989b2f29998b1c
Borja Gómez Muñoz <borja@reverscore.com>
2022-04-23 23:39:56 +0200

    Adding sentiment post endpoint

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ import logging
 from logging.config import dictConfig
 from log_config import log_config # this is your local file
 
+from pydantic import BaseModel
+class PredictionRequest(BaseModel):
+  query_string: str
+
+from transformers import pipeline
+sentiment_model = pipeline("sentiment-analysis")  
+
 dictConfig(log_config)
 logger = logging.getLogger("my-project-logger") # should be this name unless you change it in log_config.py
 
@@ -13,4 +20,10 @@ app = FastAPI()
 def perform_healthcheck():
     logging.info('Healthcheck ok')
     return {'healthcheck': 'Ok'}
-    
+
+@app.post("/predict")
+def predict(request: PredictionRequest):
+  # YOUR CODE GOES HERE
+  sentiment_query_sentence = request.query_string
+  sentiment = sentiment_model(sentiment_query_sentence)
+  return {'sentiment': sentiment}
