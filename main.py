2f6a9e892db78184ff03da47b69a06e55a9f2792
Borja Gómez Muñoz <borja@reverscore.com>
2022-04-23 23:29:39 +0200

    Logging

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from fastapi import FastAPI, status
 
+import logging
+from logging.config import dictConfig
+from log_config import log_config # this is your local file
+
+dictConfig(log_config)
+logger = logging.getLogger("my-project-logger") # should be this name unless you change it in log_config.py
+
 app = FastAPI()
 
 @app.get('/healthcheck', status_code=status.HTTP_200_OK)
 def perform_healthcheck():
-    '''
-    Simple route for the GitHub Actions to healthcheck on.
-    More info is available at:
-    https://github.com/akhileshns/heroku-deploy#health-check
-    It basically sends a GET request to the route & hopes to get a "200"
-    response code. Failing to return a 200 response code just enables
-    the GitHub Actions to rollback to the last version the project was
-    found in a "working condition". It acts as a last line of defense in
-    case something goes south.
-    Additionally, it also returns a JSON response in the form of:
-    {
-      'healtcheck': 'Everything OK!'
-    }
-    '''
-    return {'healthcheck': 'Everything OK!'}
+    logging.info('Healthcheck ok')
+    return {'healthcheck': 'Ok'}
     
